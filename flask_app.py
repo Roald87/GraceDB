29e264e4c1019795f8b6a78c8b63daf0b4d3bbe8
Roald87 <roaldruiter@gmail.com>
2019-05-01 11:33:54 +0200

    Moved bot code to separate class.

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,10 @@
-import threading
-import time
-import traceback
-
-import telepot
 from flask import Flask, request
 
-from config import API_TOKEN, get_secret, get_webhook_url
-from gwevents import Events, time_ago
+from config import get_secret
+from gracebot import GraceBot
 
 app = Flask(__name__)
-bot = telepot.Bot(API_TOKEN)
-
-bot.setWebhook(get_webhook_url(), max_connections=1)
+bot = GraceBot()
 
 @app.route('/{}'.format(get_secret()), methods=["POST"])
 def telegram_webhook():
@@ -20,103 +13,11 @@ def telegram_webhook():
         text = update["message"]["text"]
         chat_id = update["message"]["chat"]["id"]
         if '/start' in text or '/help' in text:
-            send_welcome(chat_id)
+            bot.send_welcome(chat_id)
         elif '/latest' in text:
-            send_latest(chat_id)
+            bot.send_latest(chat_id)
 
     return "OK"
-
-
-def send_welcome(chat_id: int) -> None:
-    """
-    Welcome and help message of the bot.
-
-    Parameters
-    ----------
-    chat_id : int
-        Chat to send the message to.
-
-    Returns
-    -------
-    None
-    """
-    bot.sendMessage(
-        chat_id,
-        'Get information on LIGO/Virgo gravitational wave events.\n'
-        'Use /latest to see the latest event.')
-
-
-def every(delay: int, task: object):
-    """
-    Repeat a task at an interval.
-
-    Parameters
-    ----------
-    delay : int
-        Calls the task with this interval in seconds.
-    task : object
-        The task which should be called.
-
-    Returns
-    -------
-    None
-
-    See Also
-    --------
-    https://stackoverflow.com/a/49801719/6329629
-    """
-    next_time = time.time() + delay
-    while True:
-        time.sleep(max(0, next_time - time.time()))
-        try:
-            task()
-            print("updating events")
-        except Exception:
-            traceback.print_exc()
-    # skip tasks if we are behind schedule:
-    next_time += (time.time() - next_time) // delay * delay + delay
-
-
-events = Events()
-hourly_event_updater = threading.Thread(target=lambda: every(3600, events.update_events))
-hourly_event_updater.start()
-
-
-def send_latest(chat_id: int) -> None:
-    """
-    Send the latest event to the telegram bot.
-
-    Parameters
-    ----------
-    chat_id : int
-
-    Returns
-    -------
-    None
-    """
-
-    event = events.latest()
-    distance_mean = round(event["distance_mean_Mly"])
-    distance_std = round(event["distance_std_Mly"])
-    event_type, confidence = events.get_event_type(event.name)
-    confirmed = {'s': 'Unconfirmed', 'g': 'Confirmed'}
-
-    bot.sendMessage(
-        chat_id,
-        f'*{event.name.upper()}*\n'
-        f'{time_ago(event["created"])}\n\n'
-        f'{confirmed[event.name[0].lower()]} {event_type} ({confidence:.2%}) event '
-        f'at {distance_mean} ± {distance_std} million light years.',
-        parse_mode='Markdown')
-    try:
-        image_fname = events.picture(event.name)
-        with open(image_fname, 'rb') as picture:
-            bot.sendPhoto(
-                chat_id,
-                picture
-            )
-    except FileNotFoundError:
-        pass
 
 
 if __name__ == '__main__':
---
+++ gracebot.py
@@ -0,0 +1,103 @@
+import threading
+import time
+import traceback
+
+import telepot
+
+from config import API_TOKEN, get_webhook_url
+from gwevents import Events, time_ago
+
+
+class GraceBot(object):
+
+    def __init__(self):
+        self.bot = telepot.Bot(API_TOKEN)
+        self.bot.setWebhook(get_webhook_url(), max_connections=1)
+        self.events = Events()
+        hourly_event_updater = threading.Thread(
+            target=lambda: every(3600, self.events.update_events))
+        hourly_event_updater.start()
+
+    def send_welcome(self, chat_id: int) -> None:
+        """
+        Welcome and help message of the bot.
+
+        Parameters
+        ----------
+        chat_id : int
+            Chat to send the message to.
+
+        Returns
+        -------
+        None
+        """
+        self.bot.sendMessage(
+            chat_id,
+            'Get information on LIGO/Virgo gravitational wave events.\n'
+            'Use /latest to see the latest event.')
+
+    def send_latest(self, chat_id: int) -> None:
+        """
+        Send the latest event to the telegram bot.
+
+        Parameters
+        ----------
+        chat_id : int
+
+        Returns
+        -------
+        None
+        """
+
+        event = self.events.latest()
+        distance_mean = round(event["distance_mean_Mly"])
+        distance_std = round(event["distance_std_Mly"])
+        event_type, confidence = self.events.get_event_type(event.name)
+        confirmed = {'s': 'Unconfirmed', 'g': 'Confirmed'}
+
+        self.bot.sendMessage(
+            chat_id,
+            f'*{event.name.upper()}*\n'
+            f'{time_ago(event["created"])}\n\n'
+            f'{confirmed[event.name[0].lower()]} {event_type} ({confidence:.2%}) event '
+            f'at {distance_mean} ± {distance_std} million light years.',
+            parse_mode='Markdown')
+        try:
+            image_fname = self.events.picture(event.name)
+            with open(image_fname, 'rb') as picture:
+                self.bot.sendPhoto(
+                    chat_id,
+                    picture
+                )
+        except FileNotFoundError:
+            pass
+
+def every(delay: int, task: object):
+    """
+    Repeat a task at an interval.
+
+    Parameters
+    ----------
+    delay : int
+        Calls the task with this interval in seconds.
+    task : object
+        The task which should be called.
+
+    Returns
+    -------
+    None
+
+    See Also
+    --------
+    https://stackoverflow.com/a/49801719/6329629
+    """
+    next_time = time.time() + delay
+    while True:
+        time.sleep(max(0, next_time - time.time()))
+        try:
+            task()
+            print("updating events")
+        except Exception:
+            traceback.print_exc()
+    # skip tasks if we are behind schedule:
+    next_time += (time.time() - next_time) // delay * delay + delay
