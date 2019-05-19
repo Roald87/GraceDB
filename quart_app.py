f9c5c33e9ba1aeb4d38485b1351632fa1378e98a
Roald87 <roaldruiter@gmail.com>
2019-05-19 11:26:13 +0200

    Runs locally.

---
+++ quart_app.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+
+from config import API_TOKEN, get_webhook_url
+
+# webhook settings
+WEBHOOK_URL = get_webhook_url()
+
+# webserver settings
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 8088
+
+logging.basicConfig(level=logging.INFO)
+
+loop = asyncio.get_event_loop()
+bot = Bot(token=API_TOKEN, loop=loop)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    # insert code here to run it before shutdown
+    pass
+
+
+if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp, webhook_path='',
+        on_startup=on_startup, on_shutdown=on_shutdown,
+        skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT
+    )
