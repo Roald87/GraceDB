6d571570fb2c040e50e7397f101c0b59ca748997
Roald87 <roaldruiter@gmail.com>
2019-07-13 20:26:55 +0200

    Closes #5. Shows down times over 24 hours.

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import timedelta
 from unittest import TestCase
 
 from detector import Detector
@@ -18,7 +18,7 @@ class TestHanford(TestCase):
         assert self.hanford.status == "Observing"
 
     def test_duration(self):
-        assert self.hanford.duration == time(0, 51)
+        assert self.hanford.duration == timedelta(minutes=51)
 
 
 class TestLivingston(TestCase):
@@ -32,7 +32,7 @@ class TestLivingston(TestCase):
         assert self.livingston.status == "Down"
 
     def test_duration(self):
-        assert self.livingston.duration == time(0, 2)
+        assert self.livingston.duration == timedelta(minutes=2)
 
 
 class TestVirgo(TestCase):
@@ -46,7 +46,7 @@ class TestVirgo(TestCase):
         assert self.virgo.status == "Adjusting"
 
     def test_duration(self):
-        assert self.virgo.duration == time(0, 10)
+        assert self.virgo.duration == timedelta(minutes=10)
 
 
 class TestHanford2(TestCase):
@@ -60,7 +60,7 @@ class TestHanford2(TestCase):
         assert self.hanford.status == "Down"
 
     def test_duration(self):
-        assert self.hanford.duration == time(5, 37)
+        assert self.hanford.duration == timedelta(hours=5, minutes=37)
 
 
 class TestReplacingUnderscoreInStateWithSpace(TestCase):
@@ -74,7 +74,21 @@ class TestReplacingUnderscoreInStateWithSpace(TestCase):
         assert self.detector.status == "Not locked"
 
     def test_duration(self):
-        assert self.detector.duration == time(5, 37)
+        assert self.detector.duration == timedelta(hours=5, minutes=37)
+
+
+class TestHoursOver24(TestCase):
+    def setUp(self):
+        self.detector = Detector("Virgo", source=source2)
+
+    def test_name(self):
+        assert self.detector.name == "Virgo"
+
+    def test_status(self):
+        assert self.detector.status == "Science"
+
+    def test_duration(self):
+        assert self.detector.duration == timedelta(hours=36, minutes=44)
 
 
 class TestWithoutTime(TestCase):
@@ -88,4 +102,4 @@ class TestWithoutTime(TestCase):
         assert self.detector.status == "Info too old"
 
     def test_duration(self):
-        assert self.detector.duration == time(0)
+        assert self.detector.duration == timedelta(0)
